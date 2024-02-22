778b442609b8c46eeb3590667ce3b5b54432ad2f
nyyakko <nyakonyns@gmail.com>
2024-02-21 21:07:37 -0300

    move everything to the same header.

--- errOr/include/errOr/ErrorOr.hpp
+++ errOr/include/errOr/ErrorOr.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
-#include "policies/DefaultError.hpp"
-
+#include <format>
 #include <optional>
 #include <print>
+#include <ranges>
+#include <sstream>
+#include <stacktrace>
 #include <string_view>
+#include <string_view>
+#include <string>
 
 #define TRY(expression) ({                                                          \
     auto _ = (expression);                                                          \
@@ -23,15 +27,97 @@
     *_;                                                                             \
 })
 
-#define SHOULD(expression) ({                                                       \
-    auto _ = (expression);                                                          \
-    if (_.has_error())                                                              \
-    {                                                                               \
-        std::println(stderr, "Failed because: {}", _.error().message());            \
-        std::exit(1);                                                               \
-    }                                                                               \
-    *_;                                                                             \
-})
+namespace error {
+
+class DefaultError
+{
+public:
+    constexpr DefaultError()  = default;
+    constexpr ~DefaultError() = default;
+
+    explicit constexpr DefaultError(std::string_view const message): message_m(message) {}
+    constexpr DefaultError(DefaultError const& error): message_m(error.message_m) {}
+    constexpr DefaultError(DefaultError&& error) noexcept: message_m(std::move(error.message_m)) {}
+
+    constexpr DefaultError& operator=(DefaultError&& error) noexcept
+    {
+        message_m = std::exchange(error.message_m, {});
+        return *this;
+    }
+
+    constexpr DefaultError& operator=(DefaultError const& error) noexcept
+    {
+        message_m = error.message_m;
+        return *this;
+    }
+
+    constexpr auto const& message() const { return message_m; }
+
+private:
+    std::string message_m;
+};
+
+}
+
+namespace error {
+
+class TraceableError
+{
+public:
+    constexpr TraceableError()  = default;
+    constexpr ~TraceableError() = default;
+
+    explicit constexpr TraceableError(std::string_view const format, auto&&... args):
+        message_m(std::vformat(format, std::make_format_args(std::forward<decltype(args)>(args)...))),
+        errorStack_m(std::stacktrace::current())
+    {}
+
+    constexpr TraceableError(TraceableError const& error):
+        message_m(error.message_m),
+        errorStack_m(error.errorStack_m)
+    {}
+
+    constexpr TraceableError(TraceableError&& error):
+        message_m(std::move(error.message_m)),
+        errorStack_m(std::move(error.errorStack_m))
+    {}
+
+    constexpr TraceableError& operator=(TraceableError&& error)
+    {
+        message_m    = std::exchange(error.message_m, {});
+        errorStack_m = std::exchange(error.errorStack_m, {});
+        return *this;
+    }
+
+    constexpr TraceableError& operator=(TraceableError const& error)
+    {
+        message_m    = error.message_m;
+        errorStack_m = error.errorStack_m;
+        return *this;
+    }
+
+    auto message() const
+    {
+        std::stringstream stream {};
+
+        stream << message_m << "\n" << "    Stacktrace:\n";
+
+        for (auto const& [index, stack] : errorStack_m
+                                            | std::views::drop(2)
+                                            | std::views::enumerate)
+        {
+            stream << "\t" << "[" << index << "] " << stack << '\n';
+        }
+
+        return stream.str();
+    }
+
+private:
+    std::string message_m {};
+    std::stacktrace errorStack_m {};
+};
+
+}
 
 namespace error {
 
