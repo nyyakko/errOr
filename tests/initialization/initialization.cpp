1410cce5c32de1047227a61801e304b6aad3ba22
nyyakko <nyakonyns@gmail.com>
2024-04-01 23:13:23 -0300

    add object initialization tests

---
+++ tests/initialization/initialization.cpp
@@ -0,0 +1,68 @@
+#include <gtest/gtest.h>
+
+#include <liberror/ErrorOr.hpp>
+
+#include <array>
+#include <string>
+#include <print>
+
+using namespace liberror;
+
+static constexpr auto BUFFER_SIZE = 1024;
+
+int redirect_stdout_to_buffer(std::array<char, BUFFER_SIZE>& buffer)
+{
+    fflush(stdout);
+    auto previousState = _dup(_fileno(stdout));
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    (void)freopen("NUL", "a", stdout);
+#pragma clang diagnostic pop
+    setvbuf(stdout, buffer.data(), _IOFBF, buffer.size());
+
+    return previousState;
+}
+
+void restore_stdout(int state)
+{
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    (void)freopen("NUL", "a", stdout);
+#pragma clang diagnostic pop
+    _dup2(state, _fileno(stdout));
+    setvbuf(stdout, NULL, _IONBF, BUFFER_SIZE);
+}
+
+struct S
+{
+    constexpr S(std::string) noexcept { std::println("S::S(std::string)"); }
+    constexpr ~S() noexcept { std::println("S::~S()"); }
+    constexpr S(S const&) noexcept { std::println("S::S(S const&)"); }
+    constexpr S(S&&) noexcept { std::println("S::S(S&&)"); }
+    constexpr S& operator=(S const&) noexcept { std::println("S::S operator=(S const&)"); return *this; }
+    constexpr S& operator=(S&&) noexcept { std::println("S::S operator=(S&&)"); return *this; }
+};
+
+TEST(initialization, in_place_success)
+{
+    std::array<char, BUFFER_SIZE> buffer {};
+    auto const previousState = redirect_stdout_to_buffer(buffer);
+    (void)[] -> ErrorOr<S> {
+        return "hello";
+    }();
+    restore_stdout(previousState);
+    EXPECT_STREQ(buffer.data(), "S::S(std::string)\nS::~S()\n");
+}
+
+TEST(initialization, no_extra_object_failure)
+{
+    std::array<char, BUFFER_SIZE> buffer {};
+    auto const previousState = redirect_stdout_to_buffer(buffer);
+    auto const result = [] -> ErrorOr<S> {
+        return make_error("failure");
+    }();
+    std::println("{}", result.error().message());
+    restore_stdout(previousState);
+    EXPECT_STREQ(buffer.data(), "failure\n");
+}
+
