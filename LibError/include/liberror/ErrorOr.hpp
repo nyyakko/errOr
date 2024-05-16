27c056f3e855ac19d4477bcd8cf83cd393ad613f
nyyakko <nyakonyns@gmail.com>
2024-05-16 09:24:43 -0400

    fix MUST macro

--- LibError/include/liberror/ErrorOr.hpp
+++ LibError/include/liberror/ErrorOr.hpp
@@ -25,22 +25,21 @@
 #define LIBERROR_EXP std
 #endif
 
-#define TRY(expression) ({                                                              \
-    using namespace liberror;                                                           \
-    auto&& _ = (expression);                                                            \
-    if (!_.has_value()) return make_error(_.error());                                   \
-    std::move(_).value();                                                               \
+#define TRY(expression) ({                                                                   \
+    using namespace liberror;                                                                \
+    auto&& _ = (expression);                                                                 \
+    if (!_.has_value()) return make_error(_.error());                                        \
+    std::move(_).value();                                                                    \
 })
 
-#define MUST(expression) ({                                                             \
-    using namespace liberror;                                                           \
-    auto&& _ = (expression);                                                            \
-    if (!_.has_value())                                                                 \
-    {                                                                                   \
-        liberor::println(stderr, "Aborted execution because: {}", _.error().message()); \
-        std::abort();                                                                   \
-    }                                                                                   \
-    std::move(_).value();                                                               \
+#define MUST(expression) ({                                                                  \
+    auto&& _ = (expression);                                                                 \
+    if (!_.has_value())                                                                      \
+    {                                                                                        \
+        LIBERROR_FMT::println(stderr, "Aborted execution because: {}", _.error().message()); \
+        std::abort();                                                                        \
+    }                                                                                        \
+    std::move(_).value();                                                                    \
 })
 
 namespace liberror {
